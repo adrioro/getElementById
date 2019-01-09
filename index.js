b2ce61579d075e06fe19b1a2ab3bc8c349b11fde
adrioro <artistsvillage@me.com>
2019-01-09 10:53:27 -0500

    third commit changing the color of an element.

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ var myIdVariable = document.getElementById("myId").value = "JS put this value in
 //What about if it's an element that is not an input, so it
 //does not have a value? Like a Div?
 document.getElementById("test").innerHTML = "<div><b>I replaced my old Div. This is my new Div! It has a new text and it is bold.<b></div>"
+document.getElementById("testafter").style.backgroundColor = "red";
